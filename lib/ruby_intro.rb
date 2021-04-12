3dc93bf97147fbb7d2f4d3c02bcbf4ef556f5876
esherman22 <ethan.sherman9822@gmail.com>
2021-04-12 13:05:49 -0600

    Syntax error fixes for final testing hopefully

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,16 +2,16 @@
 
 # Part 1
 
-def sum(array)
+def sum array
   return array.sum
 end
 
-def max_2_sum(array)
-  return array.empty? ? 0 : array.length == 1 ? array[0] : array.delete_at(array.max) + array.max
+def max_2_sum array
+  return array.empty? ? 0 : array.length == 1 ? array[0] : array.delete_at(array.index(array.max)) + array.max
 end
 
-def sum_to_n?(array, n)
-  return array.empty? ? false : array.length == 1 ? false : array.combination(2).to_a.select (|x| x[0] + x[1] == n).empty? ? false : true
+def sum_to_n? array, n
+  return array.empty? ? false : array.length == 1 ? false : array.combination(2).to_a.select {|x| x[0] + x[1] == n}.empty? ? false : true
 end
 
 # Part 2
@@ -20,11 +20,11 @@ def hello(name)
   return "Hello, #{name}"
 end
 
-def starts_with_consonant?(s)
+def starts_with_consonant? s
   return s.empty? ? false : s[0] !~ /[\W]A|E|I|O|U/i
 end
 
-def binary_multiple_of_4?(s)
+def binary_multiple_of_4? s
   return s.empty? ? false : s =~ /^[0|1]+$/ ? s.to_i()%4 == 0 : false
 end
 
@@ -41,9 +41,11 @@ class BookInStock
 		@price = price
 	end
 	
+	# Getters
 	attr_reader :isbn
 	attr_reader :price
 	
+	# Setters
 	attr_writer :isbn
 	attr_writer :price
 	
